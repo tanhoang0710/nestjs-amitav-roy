54aaaff7fab659e5b079efef62b82b9cc3fb4b03
tanhun <tanhoang0710@gmail.com>
2023-05-07 16:27:58 +0700

    24 Nest JS Custom exceptions and why they are useful

--- src/common/middleware/api-token-check.middleware.ts
+++ src/common/middleware/api-token-check.middleware.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, NestMiddleware } from '@nestjs/common';
+import { NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { ApiTokenPaymentException } from '../exceptions/api-token-payment.exception';
 
 export class ApiTokenCheckMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     if (req.headers['api-token'] !== 'my-token')
-      throw new BadRequestException('The token doesnt match');
+      throw new ApiTokenPaymentException();
     else next();
   }
 }
